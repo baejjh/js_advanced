be500f57059258375a342066a755e3ba7dbc0bfd
Jeannie Baye <jeanniebaye@gmail.com>
2014-12-18 14:08:16 -0800

    fixed getTime function error

--- js_balls.php
+++ js_balls.php
@@ -104,28 +104,21 @@
 	document.onclick = function(e) {
 		playground.createNewCircle(e.x,e.y);
 	}
-
-	//using immediate function to avoid global variable issues
-	( function(){
-
-		var mousedown_time;
+	var mousedown_time;
 
 		function getTime(){
 			var date = new Date();
 			return date.getTime();
 		}
 
-		document.onmousedown = function(e){
+	document.onmousedown = function(e){ //technically we don't even need the mousedown variable but we're leaving it there for now..
 			mousedown_time = getTime();
-		}
-		document.onmouseup = function(e){
-			time_pressed = getTime() - mousedown_time;
-			console.log('You held your mouse down for', time_pressed, 'miliseconds.');
-		}
-
-		//technically we don't even need the mousedown variable but we're leaving it there for now..
-	})();
-
+	}
+	document.onmouseup = function(e){
+		time_pressed = getTime() - mousedown_time;
+		console.log('You held your mouse down for', time_pressed, 'miliseconds.');
+	}
+	
 	</script>
 
 </body>
